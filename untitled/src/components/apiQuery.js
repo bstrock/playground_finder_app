95fe1411045d6f124975e8a5af5ca1b74649298e
bstrock <bstrock@wisc.edu>
2022-04-30 23:23:24 -0500

    even better

--- untitled/src/components/apiQuery.js
+++ untitled/src/components/apiQuery.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default async function apiQuery (queryLocation, queryParams) {
+export default async function apiQuery (queryLocation, queryParams, setLoadingProgress) {
 
     let inParams = {...queryLocation, ...queryParams}
 
@@ -20,20 +20,36 @@ export default async function apiQuery (queryLocation, queryParams) {
             }
         }
 
-    let response = await axios.get(`${apiUrl}/query`, {params: outParams})
+    let response = await axios.get(`${apiUrl}/query`, {
+        params: outParams,
+        responseType: 'blob',
+        onDownloadProgress: (progressEvent) => {
+            let progress = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            setLoadingProgress(progress)
+            }
+    })
         .catch((error) => {
             console.log(error)
             errorHappened = true
         })
 
     if (errorHappened) {
-        response = await axios.get(`http://localhost:8001/query`, {params: outParams})
+        response = await axios.get(`http://localhost:8001/query`, {
+            params: outParams,
+            responseType: 'blob',
+            onDownloadProgress: (progressEvent) => {
+
+                setLoadingProgress(Math.round((progressEvent.loaded * 100) / progressEvent.total))
+            }
+        })
             .catch((error) => {
                 console.log('second try')
                 console.log(error)
             })
         errorHappened = false
     }
+    const fr = new FileReader()
 
+    fr.readAsText(response.data)
     return response.data
 }
